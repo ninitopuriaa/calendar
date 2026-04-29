19c718a07a243529a78174354a7f242970b4d1d6
nini <ninitopuria.77@gmail.com>
2026-04-29 21:47:13 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const currentDate = document.querySelector(".curr-data");
+const datesTag = document.querySelector(".dates");
+
+let date = new Date();
+let currYear = date.getFullYear();
+let currMonth = date.getMonth();
+
+const months = ["January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December"];
+
+const calendar = () => {
+    let firstDayOfMonth = new Date(currYear, currMonth, 1).getDay();
+    let lastDateOfMonth = new Date(currYear, currMonth + 1, 0).getDate();
+    let lastDayOfMonth = new Date(currYear, currMonth, lastDateOfMonth).getDay();
+    let lastDateOfLastMonth = new Date(currYear, currMonth, 0).getDate();
+
+    let liTag = "";
+
+    for (let i = firstDayOfMonth; i > 0; i--) {
+        liTag += `<li class="inactive">${lastDateOfLastMonth - i + 1}</li>`;
+    }
+    for (let i = 1; i <= lastDateOfMonth; i++) {
+        let isToday = i === date.getDate() && currMonth === date.getMonth() && currYear === date.getFullYear() ? "active" : "";
+        liTag += `<li class="${isToday}">${i}</li>`;
+    }
+    for (let i = lastDayOfMonth; i < 6; i++) {
+        liTag += `<li class="inactive">${i - lastDayOfMonth + 1}</li>`;
+    }
+    currentDate.innerHTML = `${months[currMonth]} ${currYear}`;
+    datesTag.innerHTML = liTag;
+}
+calendar();
